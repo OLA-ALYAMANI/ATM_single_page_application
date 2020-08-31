29a5ead912457d64153c89832a7779d9bcd6195a
OLA13 <o-m-yamani@hotmail.com>
2020-08-31 18:15:28 +0300

    try for date

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -4,89 +4,38 @@ var userinput = {
     baln : "500"
 
 }
-$("#screen__pin").append("<span id = 'To_Right'></span>")
 
-$("<span id = 'container'></span>").appendTo('#To_Right')
-$("<span id = 'ThePin'></span>").appendTo('#container')
-$("<p id='inp_num'></p>").appendTo("#ThePin")
+// numberbutton = document.querySelectorAll(".call_number");
+function changeName(){
+    $("p").html("Good Morning " + userinput.name + ", How are you today?")
+}
+var oprationbutton = document.querySelectorAll(".Operating_buttons")
+var inPut = document.querySelector("#inp_num");
+var outPut='';
 
-$("<span id = 'pin_button'></span>").appendTo('#container')
-$("<button class='call_number' id='1' value='1'>1</button>").appendTo("#pin_button")
-$("<button class='call_number' id='2' value='2'>2</button>").appendTo("#pin_button")
-$("<button class='call_number' id='3' value='3'>3</button>").appendTo("#pin_button")
-$("<button class='call_number' id='4' value='4'>4</button>").appendTo("#pin_button")
-$("<button class='call_number' id='5' value='5'>5</button>").appendTo("#pin_button")
-$("<button class='call_number' id='6' value='6'>6</button>").appendTo("#pin_button")
-$("<button class='call_number' id='7' value='7'>7</button>").appendTo("#pin_button")
-$("<button class='call_number' id='8' value='8'>8</button>").appendTo("#pin_button")
-$("<button class='call_number' id='9' value='9'>9</button>").appendTo("#pin_button")
-$("<div class='call_number'></div>").appendTo("#pin_button")
-$("<button class='call_number' id='0' value='0'>0</button>").appendTo("#pin_button")
-$("<div class='call_number'></div>").appendTo("#pin_button")
+    $(".call_number").click(function(){
+        var value = $(this).val();
+        if (value.length === 4) {
+            
+        }
+        $("input").val(value);
+    })
 
-<<<<<<< HEAD
-var CallNum = $(".call_number")
-var inPut = $("#inp_num")
-var outPut
-
-CallNum.click(function(e){
-    for(var i=0; i<4; i++){
-    $(this).addClass("Click")
-    outPut = $(this)[i].val()
-    inPut.innerText() += outPut[i].val()
-    }
-    // if(outPut.length == 4 ){
-        
-    // }
-    // console.log($(this).val())
-})
-=======
-// var CallNum = $(".call_number")
-// var inPut = $("#inp_num").text()
-// var outPut
 
 // CallNum.click(function(e){
 //     for(var i=0; i<4; i++){
-//     $(this).val().addClass(".Click")
-//     outPut[i] += $(this)[i].text()
-//     inPut += outPut[i]
+//     $(this).addClass("Click")
+//     outPut = $(this)[i].val()
+//     inPut.innerText() += outPut[i].val()
 //     }
 //     // if(outPut.length == 4 ){
         
 //     // }
 //     // console.log($(this).val())
 // })
->>>>>>> d239ae780a543adff408f4553fb7e6762883878d
 
-//Cancle Button, Clear Button and Enter Button
-$("#container").append("<span id = 'command_span'></span>")
-$("<button class='command_button' id='clear'></button>").appendTo("#command_span")
-$("<button class='command_button' id='cancle'></button>").appendTo("#command_span")
-$("<button class='command_button' id='enter'></button>").appendTo("#command_span")
 
-$("#clear").append("<p id='Yellow'></p>")
-$("#cancle").append("<p id='Red'></p>")
-$("#enter").append("<p id='Green'></p>")
-//The Logo
-$("#screen__pin").append("<span id = 'To_Left'></span>")
-$("<span id = 'Logo'></span>").appendTo('#To_Left')
-$("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Al_Rajhi_Bank_Logo.svg/2000px-Al_Rajhi_Bank_Logo.svg.png'>").appendTo('#Logo')
-
-//Withdrawal Screen
-// $("<div id = 'Clock'></div>").appendTo("#screen__landing")
-// $("<h3 id = 'time'></h3>").appendTo("#Clock")
-
-// $("<div id = 'LogIn'></div>").appendTo("#screen__landing")
-// $("<div id = 'User_Name'></div>").appendTo("#LogIn")
-// $("<p id = 'Hi'>Good Morning Ola, How are you to day?</p>").appendTo("#User_Name")
-
-// $("<div id = 'container2'></div>").appendTo("#screen__landing")
-// $("<div id = 'Withdrawal_Nump'>SAR </div>").appendTo("#container2")
-// $("<div id = 'balancce'></div>").appendTo("#container2")
-// $("<button id = 'Withdrawal_but'></button>").appendTo("#container2")
-// $("<button id = 'deposit_but'></button>").appendTo("#container2")
-
-let container = document.querySelector("#time")
+let container = document.querySelector("#Time")
 function getTime(){
     let data = new Date()
     let year = data.getFullYear()//to make the second stel work
@@ -95,7 +44,7 @@ function getTime(){
     let min = data.getMinutes()
     let sec = data.getSeconds()
 
-time.textContent =hour + " : " + min + " : " + sec
+time.textContent = hour + " : " + min + " : " + sec;
 }
 
 function PIN(){
